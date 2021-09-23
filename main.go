8e81df2aee7645054d34b9b6cdeaec450587cad8
Cai Peishen <peishen.cai@foxmail.com>
2021-09-23 15:17:55 +0800

    form_post请求

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
 	// 测试请求
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
 	// rest风格请求
-	r.GET("/rest/:id", func(c *gin.Context) {
+	router.GET("/rest/:id", func(c *gin.Context) {
 		id, flag := c.Params.Get("id")
 		if !flag {
 			id = "未找到"
@@ -24,12 +24,26 @@ func main() {
 	})
 
 	// get请求
-	r.GET("/get", func(c *gin.Context) {
+	router.GET("/get", func(c *gin.Context) {
+		// c.Query("name")：c.Request.URL.Query().Get("name") 的简写
 		name := c.Query("name")
+		age := c.DefaultQuery("age", "18")
 		c.JSON(200, gin.H{
-			"message": name,
+			"message": name + " " + age,
 		})
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// form_post请求
+	router.POST("/form_post", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.DefaultPostForm("password", "123456") // 此方法可以设置默认值
+
+		c.JSON(200, gin.H{
+			"status":   "posted",
+			"username": username,
+			"password": password,
+		})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
